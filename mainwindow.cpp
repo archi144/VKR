da4666f8409626d7cc17930b746ab7bc00eed331
archi144 <37845382+archi144@users.noreply.github.com>
2018-10-22 20:36:36 +0300

    Добавлен переключатель
     Добавлен переключатель между 10-ной и 16-ной системами счисления

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,7 +4,11 @@
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/multiprecision/miller_rabin.hpp>
+#include <boost/integer/common_factor_rt.hpp>
 #include <QTime>
+#include <cstdlib>
+#include <ctime>
+using namespace boost::integer;
 using namespace boost::multiprecision;
 using boost::lexical_cast;
 
@@ -17,6 +21,10 @@ MainWindow::MainWindow(QWidget *parent) :
     sWindow= new Form();
     connect(sWindow,&Form::firstWindow,this,&MainWindow::show);
     connect(this,SIGNAL(set_open_keys()),this,SLOT(get_open_keys()));
+    connect(this,SIGNAL(set_closed_keys()),this,SLOT(get_closed_keys()));
+    connect(this,SIGNAL(set_closed_keys_16()),this,SLOT(get_closed_keys_16()));
+    connect(this,SIGNAL(set_open_keys_16()),this,SLOT(get_open_keys_16()));
+    srand(std::time(nullptr));
 
 }
 
@@ -25,27 +33,59 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-boost::multiprecision::uint1024_t MainWindow::bignum(QString a, QString b)
+boost::multiprecision::uint1024_t MainWindow::bignum(QString &a, QString &b)
 {
     return (QStringToBig(a)*QStringToBig(b));
 }
-boost::multiprecision::uint1024_t MainWindow::bignum_1(QString a, QString b)
+
+boost::multiprecision::uint1024_t MainWindow::bignum_1(QString &a, QString &b)
 {
     return ((QStringToBig(a)-1)*(QStringToBig(b)-1));
 }
-boost::multiprecision::uint1024_t MainWindow::QStringToBig(QString a)
+
+boost::multiprecision::uint1024_t MainWindow::QStringToBig(QString &a)
 {
     return (lexical_cast <boost::multiprecision::uint1024_t>(a.toStdString()));
 }
+
+QString MainWindow::DECstr_To_HEXstr(QString &a)
+{
+    std::stringstream ss;
+    ss<<std::hex<<QStringToBig(a);
+    std::string str;
+    ss>>str;
+    return a=QString::fromStdString(str);
+
+}
+
 void MainWindow::get_open_keys()       // слот меняет текст открытого ключа
 {
     QString e=ui->e->toPlainText();
     QString n=ui->n->toPlainText();
-    ui->Open_Keys->setText("Открытый ключ { " + e + "; " + n +"}");
+    ui->Open_Keys->setText("Открытый ключ { "+ e + "; " + n +"}");
 
 }
+void MainWindow::get_closed_keys()       // слот меняет текст закрытого ключа
+{
+    QString d=ui->d->toPlainText();
+    QString n=ui->n->toPlainText();
+    ui->Closed_Keys->setText("Закрытый ключ { "+ d + "; " + n +"}");
 
+}
+void MainWindow::get_open_keys_16()       // слот меняет текст открытого ключа
+{
+    QString e=ui->e->toPlainText();
+    QString n=ui->n->toPlainText();
+    ui->Open_Keys->setText("Открытый ключ { "+ DECstr_To_HEXstr(e) + "; " + DECstr_To_HEXstr(n) +"}");
 
+}
+void MainWindow::get_closed_keys_16()       // слот меняет текст закрытого ключа
+{
+    QString d=ui->d->toPlainText();
+    QString n=ui->n->toPlainText();
+    ui->Closed_Keys->setText("Закрытый ключ { "+ DECstr_To_HEXstr(d) + "; " + DECstr_To_HEXstr(n) +"}");
+
+}
 
 void MainWindow::on_Calculate_Button_clicked()
 {
@@ -57,14 +97,21 @@ void MainWindow::on_Calculate_Button_clicked()
     ui->n->setText(QString::fromStdString(n.str()));                                  // устанавливаю текст итогового поля
     n=bignum_1(p,q);
     ui->fi->setText(QString::fromStdString(n.str()));                               // записываю результат метода Эйлера
-  //  boost::multiprecision::uint1024_t MAX=ui->fi->toPlainText().toInt();
- //   boost::multiprecision::uint1024_t A = Random::get(0,ui->fi->toPlainText().toInt());   //случайно выбираю число от 0 до φ(n)
-//    if(isPrime(A) && MAX%A!=0)
-//    {
-
-//        ui->e->setText(QString::number(A));
-//    }
+    int i=3;
+    while((gcd(n,i))>2)
+    {
+        i+=2;
+    }
+    ui->e->setText(QString::number(i));
+    int k=0;
+    while (((k*n+1)%i)>0)
+    {
+        ++k;
+    }
+    boost::multiprecision::uint1024_t d=(k*n+1)/i;
+    ui->d->setText((QString::fromStdString(d.str())));
     emit set_open_keys();                     // вызываем сигнал устанавливающий открытые ключи в тексте
+    emit set_closed_keys();                   // вызываем сигнал устанавливающий закрытые ключи в тексте
 }
 
 void MainWindow::on_pushButton_2_clicked()
@@ -74,14 +121,6 @@ void MainWindow::on_pushButton_2_clicked()
 }
 
 
-
-void MainWindow::on_Bit_SLider_valueChanged(int value)
-{
-    ui->Bit_SLider->setSingleStep(ui->Bit_SLider->value());       // устанавливаю шаг равный значению слайдера
-    ui->Bit_label->setText(QString::number(value) + " Бит(а)");
-
-
-}
 
 
 // генерирует цифры , объединяя их в большое число и перед записью проверяет на простоту
@@ -99,7 +138,8 @@ void MainWindow::on_Generate_button_clicked()
     while (limit)
     {
         QString tempValue;
-        int x=qrand()%9+1;                                  // рандом от 1 до 9
+
+        int x=rand()%9+1;                                  // рандом от 1 до 9
             if(limit==1 && x%2==0)
             {
                 while (x%2==0)
@@ -116,4 +156,42 @@ void MainWindow::on_Generate_button_clicked()
      }
 }
     ui->Time_of_generate->setText("Время генерации (мс): "+QString::number(time.elapsed()));
+}
+
+
+void MainWindow::on_tabWidget_currentChanged(int index)
+{
+    if(index==0)
+
+    ui->tabWidget->QWidget::lower();
+    else
+        ui->tabWidget->QWidget::raise();
+}
+
+
+void MainWindow::on_Shifr_button_clicked()
+{
+   QString Text=ui->Noshifr_textEdit->toPlainText();
+//   for (int i=0;i<Text.length();i++)
+//    {
+
+//    }
+  ui->Shifr_textEdit->setPlainText(Text.toLatin1());
+
+}
+
+
+
+void MainWindow::on_check_16Hex_clicked()
+{
+    if(ui->check_16Hex->isChecked())
+    {
+        emit get_closed_keys_16();
+        emit get_open_keys_16();
+    }
+    else
+    {
+        emit get_closed_keys();
+        emit get_open_keys();
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,26 +15,30 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-  boost::multiprecision::uint1024_t  bignum(QString a, QString b);
-  boost::multiprecision::uint1024_t bignum_1(QString a, QString b);
-  boost::multiprecision::uint1024_t QStringToBig(QString a);
+  boost::multiprecision::uint1024_t  bignum(QString &a, QString &b);
+  boost::multiprecision::uint1024_t bignum_1(QString &a, QString &b);
+  boost::multiprecision::uint1024_t QStringToBig(QString &a);     //представляет строку в виде переменной uint1024_t
+  QString DECstr_To_HEXstr(QString &a);       //из 10-ной системы счисления в 16-ную
 signals:
     void set_open_keys();
-    void is_a_prime();
+    void set_closed_keys();
+    void set_open_keys_16();
+    void set_closed_keys_16();
+
 private slots:
 
     void get_open_keys();
-
-
+    void get_closed_keys();
+    void get_open_keys_16();
+    void get_closed_keys_16();
     void on_pushButton_2_clicked();
-
     void on_Calculate_Button_clicked();
-
-
-    void on_Bit_SLider_valueChanged(int value);
-
-
     void on_Generate_button_clicked();
+    void on_tabWidget_currentChanged(int index);
+
+    void on_Shifr_button_clicked();
+
+    void on_check_16Hex_clicked();
 
 private:
 
